f621c62472e84377cc7235306a9a1e18a1e07aa7
Dwayne Macgowan <dwaynemac@gmail.com>
2012-07-17 19:27:32 -0300

    initial rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Overmind::Application.config.secret_token = 'c653340941ff59bfc69b8e03b8fb88c66cbbb5e729596d6f808718cf8a97d902241c8b63ef5032100ae7fa3cec4fe3220aef681358ed9438d2a97493538b2b7a'
