90b8b4cfe63c0a75593de99ac5d1447745e546c3
Dwayne Macgowan <dwaynemac@gmail.com>
2012-11-22 14:55:12 -0300

    [#37735825] Fetch students count from contacts

--- spec/models/monthly_stat_spec.rb
+++ spec/models/monthly_stat_spec.rb
@@ -101,12 +101,33 @@ describe MonthlyStat do
   end
 
   describe ".create_from_service!" do
-    let(:school){ create(:school)}
-    before do
-      School.any_instance.stub(:fetch_stat).and_return('2')
-    end
-    it "should create a monthly stat On school" do
-      expect{MonthlyStat.create_from_service!(school,:enrollments,Date.today)}.to change{school.monthly_stats.count}.by(1)
+    let(:school){ create(:school) }
+    context "with name: students" do
+      context "for kshema schools" do
+        before do
+          school.should_receive(:fetch_stat).and_return('2')
+        end
+        it "creates a monthly stat On school" do
+          expect{MonthlyStat.create_from_service!(school,:students,Date.today)}.to change{school.monthly_stats.count}.by(1)
+        end
+        it "uses KshemaAPi" do
+          # stub for indexing.
+          MonthlyStat.create_from_service!(school,:students,Date.today)
+        end
+      end
+      context "for padma2 schools" do
+        before do
+          school.migrated_kshema_to_padma_at = Time.now
+          school.should_receive(:count_students).and_return('2')
+        end
+        it "created a monthly stat On school" do
+          expect{MonthlyStat.create_from_service!(school,:students,Date.today)}.to change{school.monthly_stats.count}.by(1)
+        end
+        it "uses PADMA Modules APi" do
+          # for indexing
+          MonthlyStat.create_from_service!(school,:students,Date.today)
+        end
+      end
     end
   end
 
