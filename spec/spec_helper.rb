40697b1ed7586c4432c46eeafe529dd7180d9bcf
Dwayne Macgowan <dwaynemac@gmail.com>
2013-09-10 03:38:05 -0300

    we can now focus specs with :focus tag in example/context

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,4 +31,8 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, :type => :controller
 
+  # Only run specs marked with :focus in metadata or all specs if none with :focus is found.
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
