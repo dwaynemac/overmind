d393b168e2dcf1fd14e2780ddca97a17d2d42e46
Dwayne Macgowan <dwaynemac@gmail.com>
2014-03-01 19:46:04 -0300

    refactored ability definition

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -16,43 +16,55 @@ class Ability
     #
     # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
 
-    user ||= User.new
+    @user = user || User.new
 
+    block_all_permitions
+    role_specific_permitions
+    user_with_enabled_padma_accounts_permitions
+
+  end
+
+  ##
+  # Warning! This uses accessible_by. Shouldn't be used in Ability intialization.
+  #
+  def schools
+    @schools ||= School.accessible_by(self)
+  end
+
+  def federations
+    @federations ||= Federation.accessible_by(self)
+  end
+
+  private
+
+  def block_all_permitions
     cannot :manage, :all
     cannot :sync, School
     cannot :see_global, MonthlyStat
     cannot :read, Ranking
+  end
 
-    if user.padma_enabled?
-      # Users have these permitions on top of those specific to their role.
-      can [:sync,:sync_year,:read,:see_detail], School, account_name: user.enabled_accounts.map(&:name)
-      accessible_schools = School.accessible_by(self)
-      if accessible_schools.count == 1
-        can :read_only_one, School
-      end
-      accessible_federations = accessible_schools.map(&:federation_id)
-      can :read, Federation, id: accessible_federations
-      if accessible_federations.count == 1
-        can :read_only_one, Federation
-      end
-      can :manage, MonthlyStat, school: {account_name: user.enabled_accounts.map(&:name) }
-      cannot :see_global, MonthlyStat # previous can :manage grants :see_global
+  def user_with_enabled_padma_accounts_permitions
+    if @user.padma_enabled?
+      can [:sync,:sync_year,:read,:see_detail], School, account_name: enabled_account_names
+      can :read, Federation, id: user_federation_ids
+      can :manage, MonthlyStat, school: {account_name: enabled_account_names }
       can :read, Ranking
     end
+  end
 
-    case user.role
+  def role_specific_permitions
+    case @user.role
       when 'admin'
-        can :manage, :all
         can :read, Ranking
         can :sync, School
         can :create, SyncRequest
         can :see_global, MonthlyStat
+        can :manage, :all
       when 'data_entry'
-        # user.user explained: first user is local user, second user ir padma_user
-        accessible_account_names = user.user.padma_accounts.map(&:name)
-        can [:read], School, account_name: accessible_account_names
-        can :read, MonthlyStat, school: { account_name: accessible_account_names }
-        can [:create, :update, :destroy], MonthlyStat, service: '', school: {account_name: accessible_account_names }
+        can [:read], School, account_name: linked_account_names
+        can :read, MonthlyStat, school: { account_name: linked_account_names }
+        can [:create, :update, :destroy], MonthlyStat, service: '', school: {account_name: linked_account_names }
       when 'council'
         can :read, Federation
         can :read, School
@@ -61,10 +73,30 @@ class Ability
         can :read, Ranking
       when 'president'
         can :read_only_one, Federation
-        can :read, Federation, id: user.federation_id
-        can :read, School, federation_id: user.federation_id
-        can :read, MonthlyStat, school: { federation_id: user.federation_id }
+        can :read, Federation, id: @user.federation_id
+        can :read, School, federation_id: @user.federation_id
+        can :read, MonthlyStat, school: { federation_id: @user.federation_id }
         can :read, Ranking
     end
+  end
+
+  # account's that user has access to in padma-accounts and are enabled
+  def enabled_account_names
+    @enabled_account_names ||= @user.enabled_accounts.map(&:name)
+  end
+
+  # account's that user has access to in padma-accounts, enabled or not.
+  def linked_account_names
+    # user.user explained: first user is local user, second user ir padma_user
+    @linked_account_names ||= @user.user.padma_accounts.map(&:name)
+  end
+
+  # schools corresponding to enabled accounts in padma
+  def user_schools
+    @user_schools ||= School.where(account_name: enabled_account_names)
+  end
+
+  def user_federation_ids
+    @user_federation_ids ||= user_schools.map(&:federation_id)
   end
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe Ability do
+
+
   let(:ability){Ability.new(user)}
+
   describe "admin" do
     let(:user){create(:user,role: 'admin')}
     it "should be able to manage all" do
-      ability.can?(:manage,:all).should be_true
+      it_can(:manage,:all)
     end
     it "can read all federations" do
       3.times{create(:federation)}
@@ -16,23 +19,26 @@ describe Ability do
   describe "council" do
     let(:user){create(:user,role: 'council')}
     it "should NOT be able to manage all" do
-      ability.can?(:manage,:all).should be_false
+      it_cannot(:manage,:all)
     end
-    it "should be able to read all federations" do
-      ability.can?(:read,Federation).should be_true
+    it "can read all federations" do
+      it_can(:read,Federation)
     end
-    it "should be able to read all schools" do
-      ability.can?(:read,School).should be_true
+    it "can read all schools" do
+      it_can(:read,School)
     end
-    it "should be able to read all monthly values" do
-      ability.can?(:read,MonthlyStat).should be_true
+    it "can read all monthly values" do
+      it_can(:read,MonthlyStat)
+    end
+    it "can read ranking" do
+      it_can(:read,Ranking)
     end
   end
 
   describe "president" do
     let(:user){create(:user, role: 'president')}
     it "should NOT be able to manage all" do
-      ability.can?(:manage,:all).should be_false
+      it_cannot(:manage,:all)
     end
     context "in a federation" do
       let(:fed){create(:federation)}
@@ -40,24 +46,25 @@ describe Ability do
         user.update_attribute(:federation_id, fed.id)
       end
       it "should be able to read his federation" do
-        ability.can?(:read,fed).should be_true
+        it_can(:read,fed)
       end
       it "should NOT be able to read other federation" do
-        ability.can?(:read,create(:federation)).should be_false
+        it_cannot(:read,create(:federation))
       end
       it "should be able to read schools from his federation" do
-        create(:school)
+        School.delete_all
+        create(:school, federation: create(:federation))
         3.times{create(:school, federation: fed)}
         School.accessible_by(ability).count.should == 3
-        ability.can?(:read,School.last).should be_true
+        it_can(:read,School.last)
       end
       it "should NOT be able to read OTHER federation stats" do
         ms = create(:monthly_stat, school: create(:school, federation: create(:federation)))
-        ability.can?(:read,ms).should be_false
+        it_cannot(:read,ms)
       end
       it "should be able to read HIS federation stats" do
         ms = create(:monthly_stat, school: create(:school, federation: fed))
-        ability.can?(:read,ms).should be_true
+        it_can(:read,ms)
       end
     end
   end
@@ -65,7 +72,7 @@ describe Ability do
   describe "without role" do
     let(:user){create(:user)}
     it "should NOT be able to manage all" do
-      ability.can?(:manage,:all).should be_false
+      it_cannot(:manage,:all)
     end
     context "without padma_accounts" do
       before do
@@ -77,61 +84,69 @@ describe Ability do
           user.update_attribute(:federation_id, fed.id)
         end
         it "should not be able to read his federation" do
-          ability.can?(:read,fed).should be_false
+          it_cannot(:read,fed)
         end
         it "should NOT be able to read other federation" do
-          ability.can?(:read,create(:federation)).should be_false
+          it_cannot(:read,create(:federation))
         end
         it "should NOT be able to read schools from his federation" do
           create(:school)
           3.times{create(:school, federation: fed)}
-          ability.can?(:read,School.last).should be_false
+          it_cannot(:read,School.last)
         end
         it "should NOT be able to read OTHER federation stats" do
           ms = create(:monthly_stat, school: create(:school, federation: create(:federation)))
-          ability.can?(:read,ms).should be_false
+          it_cannot(:read,ms)
         end
         it "should NOT be able to read HIS federation stats" do
           ms = create(:monthly_stat, school: create(:school, federation: fed))
-          ability.can?(:read,ms).should be_false
+          it_cannot(:read,ms)
         end
       end
     end
     context "with padma_accounts" do
+      let(:fed){create(:federation)}
+      let(:school){create(:school, account_name: 'this-account', federation: fed)}
       before do
+        school
         user.stub!(:enabled_accounts).and_return([PadmaAccount.new(name: 'this-account')])
+        user.padma_enabled?.should be_true
       end
-      context "in a federation" do
-        let(:fed){create(:federation)}
-        before do
-          user.update_attribute(:federation_id, fed.id)
-        end
-        it "should not be able to read his federation" do
-          ability.can?(:read,fed).should be_false
-        end
-        it "should NOT be able to read other federation" do
-          ability.can?(:read,create(:federation)).should be_false
-        end
-        it "should NOT be able to read schools from his federation" do
-          create(:school)
-          3.times{create(:school, federation: fed)}
-          ability.can?(:read,School.last).should be_false
-          School.accessible_by(ability).count.should == 0
-        end
-        it "should be able to read his school" do
-          s = create(:school, account_name: 'this-account')
-          ability.can?(:read, s).should be_true
-          School.accessible_by(ability).count.should == 1
-        end
-        it "should NOT be able to read OTHER federation stats" do
-          ms = create(:monthly_stat, school: create(:school, federation: create(:federation)))
-          ability.can?(:read,ms).should be_false
-        end
-        it "should NOT be able to read HIS federation stats" do
-          ms = create(:monthly_stat, school: create(:school, federation: fed))
-          ability.can?(:read,ms).should be_false
-        end
+      before do
+        user.update_attribute(:federation_id, fed.id)
+      end
+      it "should be able to read his federation" do
+        it_can(:read,fed)
+      end
+      it "should NOT be able to read other federation" do
+        it_cannot(:read,create(:federation))
+      end
+      it "should NOT be able to read other schools from his federation" do
+        3.times{create(:school, federation: fed)}
+        it_cannot(:read,School.last)
+        School.accessible_by(ability).count.should == 1
+      end
+      it "should be able to read his school" do
+        it_can(:read, school)
+        School.accessible_by(ability).count.should == 1
+      end
+      it "should NOT be able to read OTHER federation stats" do
+        ms = create(:monthly_stat, school: create(:school, federation: create(:federation)))
+        it_cannot(:read,ms)
+      end
+      it "should be able to read HIS federation stats" do
+        ms = create(:monthly_stat, school: school)
+        it_can(:read,ms)
       end
     end
   end
+  
+  def it_can(action,target)
+    ability.can?(action,target).should be_true
+  end
+
+  def it_cannot(action,target)
+    ability.can?(action,target).should be_false
+  end
+
 end
