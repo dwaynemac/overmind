a57ca6f39110aea75ee809c5106fb763bcda9afc
Dwayne Macgowan <dwaynemac@gmail.com>
2014-03-11 16:47:36 -0300

    add swasthya and ini dropout rate to ranking

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -10,7 +10,7 @@ class Ranking
 
   COLUMNS_FOR_VIEW = [
    [:students, :enrollments, :dropouts],
-   [:dropouts_begginers, :dropouts_intermediates],
+   [:dropouts_begginers, :dropouts_intermediates, :begginers_dropout_rate, :swasthya_dropout_rate],
    [:male_students, :female_students],
    [:aspirante_students, :sadhaka_students, :yogin_students,
     :chela_students, :graduado_students, :assistantxe_students,
