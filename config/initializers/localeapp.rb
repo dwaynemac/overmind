f2dcee21a17d22a3c96996ec6b74b0d1f3f69f78
Dwayne Macgowan <dwaynemac@gmail.com>
2014-05-14 22:39:07 -0300

    add localeapp log, only initialize localeapp in development

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,7 +1,9 @@
-require 'localeapp/rails'
+if Rails.env.development?
+  require 'localeapp/rails'
 
-if defined?(Localeapp)
-  Localeapp.configure do |config|
-    config.api_key = 'KODlDsiQWJPPUI3a1S6dxSGQW6PMeLrryL6zyaM8emd5tH3z17'
+  if defined?(Localeapp)
+    Localeapp.configure do |config|
+      config.api_key = 'KODlDsiQWJPPUI3a1S6dxSGQW6PMeLrryL6zyaM8emd5tH3z17'
+    end
   end
 end
