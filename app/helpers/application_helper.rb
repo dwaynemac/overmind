21016fc193bf2646aadf89970929469ff1a09225
Dwayne Macgowan <dwaynemac@gmail.com>
2014-07-25 12:06:21 -0300

    [#70596828] add feedback button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,18 @@ module ApplicationHelper
     end
   end
 
+  def generate_secure_doorbell_signature()
+      jsonp_secret = 'ekXywafGmn8g60VpE1UclaFPAt4cSBzl2Cpf5gNHCIhZtN6XWWZ088SZQiP48qI5'
+      timestamp = Time.now.to_i
+      token = SecureRandom.hex(50)
+
+      signature = OpenSSL::HMAC.hexdigest(
+                  OpenSSL::Digest::Digest.new('sha256'),
+                  jsonp_secret,
+                  '%s%s' % [timestamp, token])
+
+      return "timestamp: #{timestamp}, token: '#{token}', signature: '#{signature}', ".html_safe
+  end
+
+
 end
