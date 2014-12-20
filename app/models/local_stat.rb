79802c7490a82ac4881837fd10dc89b4fdd28815
Dwayne Macgowan <dwaynemac@gmail.com>
2014-12-20 18:19:28 -0300

    missing ,

--- app/models/local_stat.rb
+++ app/models/local_stat.rb
@@ -20,11 +20,11 @@ class LocalStat
     :aspirante_students_rate,
     :sadhaka_students_rate,
     :yogin_students_rate,
-    :chela_students_rate
+    :chela_students_rate,
     :begginers_dropout_rate,
     :swasthya_dropout_rate,
     :enrollment_rate,
-    :dropout_rate,
+    :dropout_rate
   ]
   include BegginersDropoutRate
   include SwasthyaDropoutRate
