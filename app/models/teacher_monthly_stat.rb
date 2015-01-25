52e1d605e0f882f50144dc6bb4ac78ad41724e5e
Dwayne Macgowan <dwaynemac@gmail.com>
2015-01-25 19:24:46 -0300

    [#86451300] calculate teacher local_Stats on sync

--- app/models/teacher_monthly_stat.rb
+++ app/models/teacher_monthly_stat.rb
@@ -38,6 +38,7 @@ class TeacherMonthlyStat < MonthlyStat
   # @param ref_date [Date]
   # @return --
   def self.sync_school_from_service(school,name,ref_date)
+
     unless STATS_BY_TEACHER.include?(name)
       raise ArgumentError, 'this stats is not available on a teacher granularity level.'
     end
@@ -46,33 +47,53 @@ class TeacherMonthlyStat < MonthlyStat
     ref_date = ref_date.to_date.end_of_month
 
     service = MonthlyStat.service_for(school,name,ref_date)
-    teachers_stats = self.get_remote_values(school,name,service,ref_date) || []
+    if service == 'overmind'
+      school.teachers.each do |teacher|
+        value = self.calculate_local_value(school,teacher,name,ref_date)
+        create_or_update(school,teacher,name,ref_date,value) if value
+      end
+    else
+      teachers_stats = self.get_remote_values(school,name,service,ref_date) || []
 
-    teachers_stats.each do |tstat|
+      teachers_stats.each do |tstat|
 
-      tstat.stringify_keys!
+        tstat.stringify_keys!
 
-      # get teacher
-      teacher = Teacher.smart_find(tstat['padma_username'],tstat['full_name'],school)
+        # get teacher
+        teacher = Teacher.smart_find(tstat['padma_username'],tstat['full_name'],school)
 
-      existing = self.where(school_id: school.id,
-                            teacher_id: teacher.id,
-                            name: name,
-                            ref_date: ref_date)
-      if existing.empty?
-        # store new
-        self.create(school_id: school.id,
-                     teacher_id: teacher.id,
-                     name: name,
-                     ref_date: ref_date,
-                     value: tstat['value'])
-      else
-        # update existing
-        teacher_stat = existing.first
-        teacher_stat.value = tstat['value']
-        teacher_stat.save
+        create_or_update(school,teacher,name,ref_date,tstat['value'])
       end
     end
+  end
+
+  def self.create_or_update(school,teacher,name,ref_date,value)
+    existing = self.where(school_id: school.id,
+                          teacher_id: teacher.id,
+                          name: name,
+                          ref_date: ref_date)
+    if existing.empty?
+      # store new
+      self.create(school_id: school.id,
+                  teacher_id: teacher.id,
+                  name: name,
+                  ref_date: ref_date,
+                  value: value)
+    else
+      # update existing
+      teacher_stat = existing.first
+      teacher_stat.value = value
+      teacher_stat.save
+    end
+  end
+
+  def self.calculate_local_value(school,teacher,stat_name,ref_date)
+    ls = LocalStat.new(name: stat_name,
+                       ref_date: ref_date,
+                       school: school,
+                       teacher: teacher
+                      )
+    ls.value
   end
 
   # Will delegate fetching value to a corresponding module according to
