7dcc75656eaa4c4d6fe0242867e4e88af3ebca2a
Dwayne Macgowan <dwaynemac@gmail.com>
2015-02-21 20:00:36 -0300

    precompile teacher_rankings.js

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,7 +47,7 @@ Overmind::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(schools.js federations.js monthly_stats.js users.js rankings.js reports.js reports.css)
+  config.assets.precompile += %w(schools.js federations.js monthly_stats.js users.js rankings.js reports.js reports.css teacher_rankings.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
