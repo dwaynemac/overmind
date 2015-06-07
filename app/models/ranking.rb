026a97fd9a44bdfd90449b148b3e58ab8dcba9df
Dwayne Macgowan <dwaynemac@gmail.com>
2015-06-07 18:57:40 -0300

    add average age to ranking

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -13,6 +13,7 @@ class Ranking
    [:enrollment_rate, :dropout_rate],
    [:dropouts_begginers, :dropouts_intermediates, :begginers_dropout_rate, :swasthya_dropout_rate],
    [:male_students, :female_students, :male_students_rate],
+   [:students_average_age],
    [:aspirante_students, :sadhaka_students, :yogin_students,
     :chela_students, :graduado_students, :assistantxe_students,
     :professor_students, :master_students],
