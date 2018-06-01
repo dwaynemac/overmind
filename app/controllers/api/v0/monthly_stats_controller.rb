5cc2a68702fe98433c9f10440ad8a966da445349
Dwayne Macgowan <dwaynemac@gmail.com>
2018-06-01 01:47:00 +0000

    Revert "[#156789514] avoid validation problem on ''"
    This reverts commit 948eb560e514a69cc31a5652cd5d1eca80af01e4.

--- app/controllers/api/v0/monthly_stats_controller.rb
+++ app/controllers/api/v0/monthly_stats_controller.rb
@@ -103,14 +103,7 @@ class Api::V0::MonthlyStatsController < Api::V0::ApiController
   private
 
   def is_a_teacher_stat?
-    if params[:monthly_stat]
-      if params[:monthly_stat][:teacher_username].blank?
-        params[:monthly_stat].delete(:teacher_username) # delete in case we had '' value
-        false
-      else
-        true # we have teacher_username -> its a teacher_stat
-      end
-    end
+    params[:monthly_stat] && !params[:monthly_stat][:teacher_username].blank?
   end
 
   # Finds MonthlyStat duplicate
