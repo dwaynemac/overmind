0ef553dd0da70686a85f19d339761190cf21a36c
Dwayne Macgowan <dwaynemac@gmail.com>
2018-09-28 16:14:55 -0300

    live profit calculation

---
+++ app/assets/javascripts/ics_controller.js
@@ -0,0 +1,47 @@
+/* global Stimulus */
+/* global $ */
+(() => {
+  const application = Stimulus.Application.start();
+  
+  application.register("ics", class extends Stimulus.Controller {
+
+    static get targets(){
+      return ["gross_income","expenses","profit","profitError"];
+    }
+    
+    initialize(){
+      console.log("initialized ics");
+    }
+
+    updateProfit(){
+      console.log("updating profit");
+      var gross_income = parseInt(this.gross_incomeTarget.value);
+      var expenses = parseInt(this.expensesTarget.value);
+      this.profitTarget.value = gross_income - expenses;
+      this.validateProfit()
+    }
+    
+    validateProfit(){
+
+      var gross_income = parseInt(this.gross_incomeTarget.value);
+      var expenses = parseInt(this.expensesTarget.value);
+      var profit = parseInt(this.profitTarget.value);
+
+      if( (gross_income - expenses - profit) != 0 )
+        this.setProfitError("check!!");
+      else
+        this.setProfitError("");
+
+    }
+
+    setProfitError(txt){
+      if(txt != "")
+        this.profitTarget.classList.add("ic_missing");
+      else
+        this.profitTarget.classList.remove("ic_missing");
+      this.profitErrorTarget.innerText = txt;
+    }
+
+  });
+
+})();
