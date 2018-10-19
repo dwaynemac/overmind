1d24d937dd4d17dec50fd84231bed91827bf43ea
Dwayne Macgowan <dwaynemac@gmail.com>
2018-10-19 11:36:58 -0300

    [#161219708] enrollment_rate special reduction

--- lib/local_stat/enrollment_rate.rb
+++ lib/local_stat/enrollment_rate.rb
@@ -1,9 +1,9 @@
 class LocalStat
   module EnrollmentRate
 
-    def calculate_enrollment_rate
-      e = value_for(:enrollments)
-      i = value_for(:p_interviews)
+    def calculate_enrollment_rate(options={})
+      e = options[:enrollments] || value_for(:enrollments)
+      i = options[:p_interviews] || value_for(:p_interviews)
 
       if i && e && i > 0
         v = ((e.to_f / i.to_f)*10000).to_i
@@ -15,6 +15,24 @@ class LocalStat
     
     def enrollment_rate_dependencies
       [:enrollments, :p_interviews]
+    end
+
+    def reduce_enrollment_rate(scope)
+      if scope.nil?
+        raise "need scope to reduce enrollment_rate"
+      end
+      cache_key = "reduced_enrollment_rate_#{Digest::SHA256.hexdigest(scope.to_sql)}"
+      ret = Rails.cache.read(cache_key)
+      if ret.nil?
+        p_interviews_sum = scope.where(name: :p_interviews).sum(:value)
+        enrollments_sum = scope.where(name: :enrollments).sum(:value)
+        ret = calculate_enrollment_rate(enrollments: enrollments_sum, p_interviews: p_interviews_sum)
+        unless ret.nil?
+          Rails.cache.write(cache_key, ret, expires_in: 5.minutes)
+        end
+      end
+      ret
+
     end
   end
 end
