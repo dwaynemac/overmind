16e3a31026e03a33950ca460897582975889aaa4
Dwayne Macgowan <dwaynemac@gmail.com>
2019-04-05 21:07:11 -0300

    show former team members if with stats

---
+++ app/helpers/teacher_ranking_helper.rb
@@ -0,0 +1,12 @@
+module TeacherRankingHelper
+  def teacher_is_currently_in_team?(teacher)
+    usernames = @school.account.users.map(&:username)
+    u = PadmaUser.find teacher.try(:username)
+    u && u.username.in?(usernames)
+  end
+
+  def non_zero_stats?(stats)
+    debugger
+    stats.sum(&:value) > 0
+  end
+end
