69b312b4a83d1a3602ce6a565c9946f660eb47c0
Dwayne Macgowan <dwaynemac@gmail.com>
2019-04-07 16:37:49 -0300

    agrego fila "sin instructor"

--- app/helpers/teacher_ranking_helper.rb
+++ app/helpers/teacher_ranking_helper.rb
@@ -8,4 +8,10 @@ module TeacherRankingHelper
   def non_zero_stats?(stats)
     stats && ( stats.sum(&:value) > 0 )
   end
+
+  def any_diff?(acums,teachers_ranking)
+    !teachers_ranking.column_names.select do |name|
+      !MonthlyStat.is_a_rate?(name) && ((acums[name] || 0) - (teachers_ranking.school_stats[@school.id]["month"][name.to_s].try(:value) || 0) != 0)
+    end.empty?
+  end
 end
