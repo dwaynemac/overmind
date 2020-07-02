7db40e0a5646b43e54bc0ab6598898472f0a0bf8
Dwayne Macgowan <dwaynemac@gmail.com>
2020-07-01 21:26:43 -0300

    remove nucleo references from schools_synchronizer.rb

--- lib/schools_synchronizer.rb
+++ lib/schools_synchronizer.rb
@@ -12,41 +12,26 @@ class SchoolsSynchronizer
     accounts.each do |account|
 
       school = School.find_by_account_name(account.name)
-      tried_to_find_by_nucleo_id = false
-      if school.nil?
-        school = School.find_by_nucleo_id(account.nucleo_id)
-        tried_to_find_by_nucleo_id = true
-      end
-
       if school.nil?
         School.create name: get_full_name(account),
-                      nucleo_id: account.nucleo_id,
                       account_name: account.name,
                       federation_id: get_federation_id(account), 
                       migrated_kshema_to_padma_at: account.migrated_to_padma_on
       else
-        if tried_to_find_by_nucleo_id
-          if school.account_name.blank?
-            school.update_attribute(:account_name, account.name)
-          else
-            Rails.logger.warn "School(##{school.name}-#{school.id}) has account_name for one account but nucleo_id for another"
-          end
-        end
         school.update_attributes(name: get_full_name(account),
                                  federation_id: get_federation_id(account)) if school.name.blank?
       end
     end
     
-    # TODO disable until nucleo is online again --
-    # School.all.each{|s| s.nucleo_enabled? } # cache nucleo_enabled on attribute: cached_nucleo_enabled
     School.all.each{|s| s.padma_enabled? } # cache padma_enabled on attribute: cached_padma_enabled
   end
 
   private
   
   def self.get_federation_id (account)
+    return nil if account.federation_nucleo_id.nil?
     federation = Federation.find_by_nucleo_id(account.federation_nucleo_id)
-    federation_id = federation.nil? ? nil : federation.id
+    federation.nil? ? nil : federation.id
   end
 
   def self.get_full_name (account)
