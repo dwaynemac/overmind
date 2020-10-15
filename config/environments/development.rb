8d21e8699e92cc0f3949001dc73505b2766de02c
Alex Falke <afalkear@gmail.com>
2020-10-15 16:53:55 -0300

    remove deprecated option whiny nils

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,9 +6,6 @@ Overmind::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -44,6 +41,6 @@ Overmind::Application.configure do
       ENV[key.to_s] = value
     end if File.exists?(env_file)
   end
-  
+
   config.log_level = :debug
 end
