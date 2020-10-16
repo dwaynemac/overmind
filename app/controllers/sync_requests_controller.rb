1f15507d91972e27e1dfaac8b378ebbe31297a72
Alex Falke <afalkear@gmail.com>
2020-10-16 12:46:45 -0300

    only load for new

--- app/controllers/sync_requests_controller.rb
+++ app/controllers/sync_requests_controller.rb
@@ -1,6 +1,6 @@
 class SyncRequestsController < ApplicationController
 
-  before_filter :load_sync_request
+  before_filter :load_sync_request, only: [:create]
   load_and_authorize_resource :school
   load_and_authorize_resource through: :school
 
