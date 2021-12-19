491e2c5c012ffb8fb3d73c2379adfa31847e0d6c
Dwayne Macgowan <dwaynemac@gmail.com>
2021-12-19 17:11:49 -0300

    specs monkey patch

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,3 +9,18 @@ require 'rspec/autorun'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
